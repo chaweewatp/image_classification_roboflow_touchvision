c3e0e90fb1cf73d1def29ebb1efddd98931b4dbd
pornchai <chaweewat.p@gmail.com>
2025-02-24 17:41:03 +0700

    add files

---
+++ 1_list_env.py
@@ -0,0 +1,21 @@
+import os
+print('os: %s' % os.__version__)
+
+
+import roboflow
+print('roboflow: %s' % roboflow.__version__)
+
+import torch
+print('torch: %s' % torch.__version__)
+
+import torchvision
+print('torchvision: %s' % torchvision.__version__)
+
+import numpy
+print('numpy: %s' % numpy.__version__)
+
+import matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+
+import colorama
+print('colorama: %s' % colorama.__version__)
